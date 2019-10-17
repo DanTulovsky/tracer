6f59b98a41e39d034dbaada1eb2ca74280df26cd
Dan Tulovsky <dant@wetsnow.com>
2019-10-16 20:28:40 -0400

    ray intersect with sphere

--- tracer/ray.go
+++ tracer/ray.go
@@ -11,7 +11,7 @@ func NewRay(o Point, d Vector) Ray {
 	return Ray{Origin: o, Dir: d}
 }
 
-// Position returns the position of the point following this ray at time t
+// Position returns the position of the point, set at r.Origin, following this ray at time t
 func (r Ray) Position(t float64) Point {
 	return r.Origin.AddVector(r.Dir.Scale(t))
 }
--- tracer/sphere.go
+++ tracer/sphere.go
@@ -1,9 +1,19 @@
 package tracer
 
+import (
+	"math"
+	"sort"
+)
+
 // Sphere is a spherical object, implement Object
 type Sphere struct {
 	Center Point
 	Radius float64
+}
+
+// NewUnitSphere returns a new Sphere centered at the origin with r=1
+func NewUnitSphere() Sphere {
+	return Sphere{Center: NewPoint(0, 0, 0), Radius: 1}
 }
 
 // NewSphere returns a new Sphere
@@ -13,5 +23,30 @@ func NewSphere(c Point, r float64) Sphere {
 
 // IntersectWith returns the 't' values of Ray r intersecting with the Sphere in sorted order
 func (s Sphere) IntersectWith(r Ray) []float64 {
-	return []float64{0}
+
+	t := []float64{}
+
+	// vecto from sphere's center to ray origin
+	sphereToRay := r.Origin.SubPoint(s.Center)
+
+	a := r.Dir.Dot(r.Dir)
+	b := 2 * r.Dir.Dot(sphereToRay)
+	c := sphereToRay.Dot(sphereToRay) - 1
+
+	// discriminant
+	d := math.Pow(b, 2) - 4*a*c
+
+	switch {
+	// no intersection
+	case d < 0:
+		return t
+	}
+
+	// one intersection means ray hits at tangent
+	t = append(t, (-b-math.Sqrt(d))/(2*a))
+	t = append(t, (-b+math.Sqrt(d))/(2*a))
+
+	sort.Float64s(t)
+
+	return t
 }
