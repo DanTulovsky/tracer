de065003f6a1ea5165ef735c380acde2600813c7
Dan Tulovsky <dant@wetsnow.com>
2019-10-20 10:39:10 -0400

    planes

---
+++ tracer/plane.go
@@ -0,0 +1,54 @@
+package tracer
+
+import (
+	"math"
+	"sort"
+
+	"github.com/DanTulovsky/tracer/constants"
+)
+
+// Plane implment a plan in xz extending infinitely in both x and z dimensions
+type Plane struct {
+	Shape
+}
+
+// NewPlane returns a new plane
+func NewPlane() *Plane {
+
+	return &Plane{
+		Shape: Shape{
+			transform: IdentityMatrix(),
+			material:  NewDefaultMaterial(),
+		},
+	}
+}
+
+// NormalAt returns the normal vector at the given point on the surface of the plane
+func (pl *Plane) NormalAt(p Point) Vector {
+	on := NewVector(0, 1, 0)
+
+	// common calculation to all shapes
+	// world normal
+	wn := on.TimesMatrix(pl.Transform().Submatrix(3, 3).Inverse().Transpose())
+
+	return wn.Normalize()
+}
+
+// IntersectWith returns the 't' values of Ray r intersecting with the plane in sorted order
+func (pl *Plane) IntersectWith(r Ray) Intersections {
+
+	t := Intersections{}
+
+	//  common calculation for all shapes
+	r = r.Transform(pl.transform.Inverse())
+
+	// parallel or coplanar
+	if math.Abs(r.Dir.Y()) < constants.Epsilon {
+		return t
+	}
+
+	t = append(t, NewIntersection(pl, -r.Origin.Y()/r.Dir.Y()))
+	sort.Sort(byT(t))
+
+	return t
+}
