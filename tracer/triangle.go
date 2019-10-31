8ec35da45f25b1a73f4b9a22d4d9b79092b7840a
Dan Tulovsky <dant@wetsnow.com>
2019-10-30 22:54:59 -0400

    triangles

--- tracer/triangle.go
+++ tracer/triangle.go
@@ -1,8 +1,9 @@
 package tracer
 
 import (
-	"log"
 	"math"
+
+	"github.com/DanTulovsky/tracer/constants"
 )
 
 // Triangle is a triangle defined by 3 points in 3d space
@@ -35,10 +36,40 @@ func NewTriangle(p1, p2, p3 Point) *Triangle {
 	return t
 }
 
+// IntersectWith returns the 't' value of Ray r intersecting with the triangle in sorted order
+func (t *Triangle) IntersectWith(r Ray) Intersections {
+	xs := NewIntersections()
+
+	dirCrossE2 := r.Dir.Cross(t.E2)
+	d := t.E1.Dot(dirCrossE2)
+	if math.Abs(d) < constants.Epsilon {
+		// ray parallel to surface of triangle
+		return xs
+	}
+
+	f := 1.0 / d
+	p1ToOrigin := r.Origin.SubPoint(t.P1)
+	u := f * p1ToOrigin.Dot(dirCrossE2)
+	if u < 0 || u > 1 {
+		// ray passes beyond p1-p3 edge
+		return xs
+	}
+
+	// ray misses p1-p2 and p2-p3 edges
+	oCrossE1 := p1ToOrigin.Cross(t.E1)
+	v := f * r.Dir.Dot(oCrossE1)
+	if v < 0 || (u+v) > 1 {
+		return xs
+	}
+
+	tval := f * t.E2.Dot(oCrossE1)
+	xs = append(xs, NewIntersection(t, tval))
+	return xs
+}
+
 // NormalAt returns the normal of the triangle at the given point
 func (t *Triangle) NormalAt(p Point) Vector {
 	// world normal
-	log.Println(t.Normal)
 	return t.Normal.NormalToWorldSpace(t)
 }
 
@@ -56,4 +87,26 @@ func (t *Triangle) Bounds() Bound {
 		Min: NewPoint(minX, minY, minZ),
 		Max: NewPoint(maxX, maxY, maxZ),
 	}
+}
+
+// calculateBounds calculates the bounding box of the shape
+func (t *Triangle) calculateBounds() {
+	minX := math.Min(t.P1.X(), math.Min(t.P2.X(), t.P3.X()))
+	minY := math.Min(t.P1.Y(), math.Min(t.P2.Y(), t.P3.Y()))
+	minZ := math.Min(t.P1.Z(), math.Min(t.P2.Z(), t.P3.Z()))
+
+	maxX := math.Max(t.P1.X(), math.Max(t.P2.X(), t.P3.X()))
+	maxY := math.Max(t.P1.Y(), math.Max(t.P2.Y(), t.P3.Y()))
+	maxZ := math.Max(t.P1.Z(), math.Max(t.P2.Z(), t.P3.Z()))
+
+	t.bound = Bound{
+		Min: NewPoint(minX, minY, minZ),
+		Max: NewPoint(maxX, maxY, maxZ),
+	}
+}
+
+// PrecomputeValues precomputes some values for render speedup
+func (t *Triangle) PrecomputeValues() {
+	// calculate group bounding box
+	t.calculateBounds()
 }
