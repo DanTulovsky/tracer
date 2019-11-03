d980a67f168fee91a88c04d7eba6ae630ec4ccba
Dan Tulovsky <dant@wetsnow.com>
2019-11-02 20:31:02 -0400

    opengl

--- examples/glfw/main.go
+++ examples/glfw/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"log"
 	"runtime"
+
+	"github.com/go-gl/gl/v4.1-core/gl" // OR: github.com/go-gl/gl/v2.1/gl
 	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
@@ -9,6 +12,26 @@ func init() {
 	// This is needed to arrange that main() runs on main thread.
 	// See documentation for functions that are only allowed to be called from the main thread.
 	runtime.LockOSThread()
+}
+
+func initOpenGL() uint32 {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL version", version)
+
+	prog := gl.CreateProgram()
+	gl.LinkProgram(prog)
+	return prog
+}
+
+func draw(window *glfw.Window, prog uint32) {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(prog)
+
+	glfw.PollEvents()
+	window.SwapBuffers()
 }
 
 func main() {
@@ -25,9 +48,11 @@ func main() {
 
 	window.MakeContextCurrent()
 
+	program := initOpenGL()
+
 	for !window.ShouldClose() {
 		// Do OpenGL stuff.
-		window.SwapBuffers()
-		glfw.PollEvents()
+		draw(window, program)
+
 	}
 }
