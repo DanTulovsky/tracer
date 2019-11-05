420c9dc41d976a83e075351bae5af04cc25f78cc
Dan Tulovsky <dant@wetsnow.com>
2019-11-05 15:32:59 -0500

    updates

--- main.go
+++ main.go
@@ -1269,7 +1269,7 @@ func main() {
 	// https://octolinker-demo.now.sh/mokiat/go-data-front
 
 	dir := fmt.Sprintf(path.Join(utils.Homedir(), "go/src/github.com/DanTulovsky/tracer/obj"))
-	f := path.Join(dir, "complex-smooth2.obj")
+	f := path.Join(dir, "test6-smooth.obj")
 	objParse(f)
 
 	if *memprofile != "" {
